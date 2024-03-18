1e93279be187d66eac977bebaeef8a048d3a2bba
JRP <jrpacheco.dev@gmail.com>
2024-03-18 09:38:54 +0000

    Update dependencies and add code for data fetching

--- apps/vite/env.ts
+++ apps/vite/env.ts
@@ -1,11 +1,11 @@
 // env.ts
 import { defineConfig } from "@julr/vite-plugin-validate-env";
-// import { z } from "zod";
+import { z } from "zod";
 
 export default defineConfig({
 	debug: true,
 	validator: "zod",
-	// schema: {
-	// 	VITE_sv: z.string(),
-	// },
+	schema: {
+		VITE_sv: z.string(),
+	},
 });
--- lessons/2 - React/Helpfull Code/dataFetching.tsx
+++ lessons/2 - React/Helpfull Code/dataFetching.tsx
@@ -1,14 +1,40 @@
 // import { createFileRoute } from "@tanstack/react-router";
 // import { useBreedList } from "../api/queries/breedQueries.js";
 // import { useState, useEffect } from "react";
-// import { Dog, getDogs } from "../api/dogAPI.js";
+// import { Dog, addDog, getDogs } from "../api/dogAPI.js";
+// import { useMutation } from "@tanstack/react-query";
 
 // export const Route = createFileRoute("/dataFetching")({
 // 	component: DogShowcase,
 // });
 
 // function DogShowcase() {
-// 	const { data: breedList, error, isLoading, refetch } = useBreedList();
+// 	const {
+// 		data: breedList,
+// 		error,
+// 		isLoading,
+// 		refetch,
+// 		isFetching,
+// 	} = useBreedList();
+
+// 	const { mutate } = useMutation({
+// 		mutationFn: ({
+// 			name,
+// 			breed,
+// 			age,
+// 		}: {
+// 			name: string;
+// 			breed: string;
+// 			age: number;
+// 		}) => addDog(name, breed, age),
+// 		onSuccess: (data) => {
+// 			console.log(data);
+// 			refetch();
+// 		},
+// 		onError: (error) => {
+// 			console.log(error);
+// 		},
+// 	});
 
 // 	//showcase the same but using useffect
 // 	// const [breedList, setBreedList] = useState<Dog[]>();
@@ -52,10 +78,26 @@
 // 			>
 // 				{breedList?.map((breed) => (
 // 					<li key={breed.id}>
-// 						{breed.name}, Age {breed.age}
+// 						{breed.breed} - {breed.name}, Age {breed.age}
 // 					</li>
 // 				))}
 // 			</ul>
+
+// 			{isFetching && <p>Fetching...</p>}
+
+// 			<button
+// 				type="button"
+// 				className="mt-4 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 transition-colors duration-300 ease-in-out"
+// 				onClick={() =>
+// 					mutate({
+// 						age: 3,
+// 						breed: "poodle",
+// 						name: "fluffy",
+// 					})
+// 				}
+// 			>
+// 				Add Dog
+// 			</button>
 
 // 			<button type="button" onClick={() => refetch()}>
 // 				Refetch
