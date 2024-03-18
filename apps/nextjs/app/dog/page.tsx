9cf4cac8c7b78c40f0a6c9ddb16caac60a156cd0
JRP <jrpacheco.dev@gmail.com>
2024-03-18 09:36:43 +0000

    Add dog page and fetch dog data

---
+++ apps/nextjs/app/dog/page.tsx
@@ -0,0 +1,4 @@
+const Page = () => {
+	return <h1>I am dog</h1>;
+};
+export default Page;
--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,8 +1,20 @@
+import { dogs } from "@repo/database";
+import db from "@repo/database/dist/database";
 
-export default function Page(): JSX.Element {
-  return (
-    <div>
-      <h1>Page</h1>
-    </div>
-  );
+export default async function Page(): Promise<JSX.Element> {
+	const doog = await db.select().from(dogs);
+
+	return (
+		<div>
+			{doog.map((d) => {
+				return (
+					<div>
+						<h1>{d.name}</h1>
+						<p>{d.breed}</p>
+						<p>{d.age}</p>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
