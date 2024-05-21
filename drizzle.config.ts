ed1cebc28e9530f5eae438f1aa2549f376dec755
JRP <jrpacheco.dev@gmail.com>
2024-05-21 12:57:46 +0100

    update drizzle kit

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit'
-
-export default defineConfig({
-    schema: "./packages/db/db.ts",
-     driver: 'better-sqlite',
-     dbCredentials: {
-         url: './packages/db/test.db',
-     },
-   })
--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
 	schema: "./schema.ts",
 	driver: "libsql",
+	dialect: "sqlite",
 	dbCredentials: {
 		url: "file:../../packages/database/sqlite.db",
 	},
