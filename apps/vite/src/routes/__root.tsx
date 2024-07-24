c229fc04ccee11f0f295d44dd8ead1e8dd26d8c2
JRP <jrpacheco.dev@gmail.com>
2024-07-24 16:07:21 +0100

    chore: Update TanStackRouterDevtools import and lazy loading

--- apps/vite/src/routes/__root.tsx
+++ apps/vite/src/routes/__root.tsx
@@ -1,22 +1,34 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 import { Outlet, createRootRouteWithContext } from "@tanstack/react-router";
 import type { QueryClient } from "@tanstack/react-query";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import Navbar from "@/components/Navbar";
+import { lazy } from "react";
 
 export const Route = createRootRouteWithContext<{
-	queryClient: QueryClient;
+  queryClient: QueryClient;
 }>()({
-	component: Layout,
+  component: Layout,
 });
 
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((res) => ({
+          default: res.TanStackRouterDevtools,
+          // For Embedded Mode
+          // default: res.TanStackRouterDevtoolsPanel
+        })),
+      );
+
 function Layout() {
-	return (
-		<div className="container">
-			<Navbar />
-			<Outlet />
-			<TanStackRouterDevtools />
-		</div>
-	);
+  return (
+    <div className="container">
+      <Navbar />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </div>
+  );
 }
 export default Layout;
