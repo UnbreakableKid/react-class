43ff0f0259a0912317f7916968a7d9619135c863
JRP <jrpacheco.dev@gmail.com>
2025-02-11 20:12:55 +0000

    chore: Update dependencies for database and nextjs, and improve drizzle configuration

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
-	schema: "./schema.ts",
-	driver: "libsql",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: "file:../../packages/database/sqlite.db",
-	},
-	strict: true,
+  schema: "./schema.ts",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: "file:../../packages/database/sqlite.db",
+  },
+  strict: true,
 });
